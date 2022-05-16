b70b5a0d01247f4c1eb135014d5c7d4ff5e986ff
Pashylik <Pashylik@gmail.com>
2022-05-17 00:09:29 +0300

    HW1

---
+++ HW1.py
@@ -0,0 +1,5 @@
+name = "Pavel"
+color = "Turquoise"
+musical_group = "Simple plan"
+movie = "The Fifth Element"
+print("My name is ", name," . ", "My favorite color is ", color,", " "musical group is", musical_group,", " "movie is", movie, sep="")
