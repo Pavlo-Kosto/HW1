5a447a2092036db8f9b1ee0f746bd19b4fabe0c1
Pashylik <Pashylik@gmail.com>
2022-05-19 01:32:06 +0300

    HW2

--- HW1.py
+++ HW1.py
@@ -3,3 +3,9 @@ color = "Turquoise"
 musical_group = "Simple plan"
 movie = "The Fifth Element"
 print("My name is ", name," . ", "My favorite color is ", color,", " "musical group is", musical_group,", " "movie is", movie, sep="")
+# Написать список мужских и женских имен, вывести только женские имена.
+names = ["Жанна", "Паша", "Марина", "Гена"]
+print(names[0::2])
+# Написать генератор списка, который создаст список, состоящий только из элементов с нечётным количеством цифр.
+list1 = [i for i in range(0, 110) if i < 10 or i > 99]
+print(list1)
