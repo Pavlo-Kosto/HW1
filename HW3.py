685c66ff3545bd3948cdd907e8a3638e1e3aa9e1
Pavlo-Kosto <80484535+Pavlo-Kosto@users.noreply.github.com>
2022-05-24 21:33:47 +0300

    HW3 update

--- HW3.py
+++ HW3.py
@@ -3,4 +3,14 @@ import turtle
 for i in range(4):
     turtle.forward(50)
     turtle.left(90)
+
+turtle.penup()
+turtle.forward(100)
+turtle.pendown()
+
+for i in range(2):
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
 turtle.exitonclick()
