e069e83560ca61519bf8329c1e17224800bc4567
Pashylik <Pashylik@gmail.com>
2022-05-28 01:40:47 +0300

    HW3 update

--- HW3.py
+++ HW3.py
@@ -1,18 +1,33 @@
-import turtle
+# import turtle
+#
+# for i in range(4):
+#     turtle.forward(50)
+#     turtle.left(90)
+#
+# turtle.penup()
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.left(90)
+# turtle.pendown()
+#
+# for i in range(2):
+#     turtle.forward(100)
+#     turtle.left(90)
+#     turtle.forward(50)
+#     turtle.left(90)
+# turtle.exitonclick()
 
-for i in range(4):
-    turtle.forward(50)
-    turtle.left(90)
+# Дополняю домашку из заданий в презентации.
+# Пароль
+password = "dodo"
+man1 = input("Введите пароль")
+try_p = 1
+while man1 != password:
+    try_p += 1
+    man1 = input("Введите пароль")
+    if try_p ==5:
+        print("До встречи")
+        break
+if man1 == password:
+    print("красавчиг")
 
-turtle.penup()
-turtle.right(90)
-turtle.forward(100)
-turtle.left(90)
-turtle.pendown()
-
-for i in range(2):
-    turtle.forward(100)
-    turtle.left(90)
-    turtle.forward(50)
-    turtle.left(90)
-turtle.exitonclick()
